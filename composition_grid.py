cb02ede8e08d325d87b470b2d2870266e3caab83
hfComp <Hila.flumin@gmail.com>
2021-08-02 17:02:42 +0300

    added set concentration factor function

---
+++ composition_grid.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Aug 2 12:36:05 2021
+
+@author: hila
+"""
+import os
+import numpy as np
+import pandas as pd
+import memly
+
+def set_concentration_factor(x):
+    x["lipid concentration"]
