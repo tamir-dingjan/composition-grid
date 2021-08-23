31a46321da52cb2cbb7272f1277c221568c2898d
hfComp <Hila.flumin@gmail.com>
2021-08-23 15:14:35 +0300

    added set concentration factor function
    updated set concentration factor function; added the set concentration value function and setting of the new composition grid csv.
    
    updated the setting of the new file and the setting of the concentration value and factor according to the wanted checkup files and bugs.
    
    debugged the set_concentrations_factor function and the creation of new_grid so it actually works.

--- composition_grid.py
+++ composition_grid.py
@@ -10,21 +10,21 @@ import numpy as np
 import pandas as pd
 
 
-def set_concentration_factor(x, the_wanted_lipid, new_con):
+def set_concentration_factor(x, changed_lipid, new_con):
     # finds the new concentration factor for one wanted lipid
-    if x["ID"] == the_wanted_lipid:
-        total_sum = np.sum(x["total_concentration"])
-        factor = (total_sum-new_con*x["total_concentration"])/(total_sum-x["total_concentration"])
-        return factor
+    old_con = x.loc[x['ID'] == changed_lipid, ['total_concentration']]
+    total_sum = np.sum(x["total_concentration"])
+    factor = (total_sum-new_con*old_con)/(total_sum-old_con)
+    print(factor.at[87, 'total_concentration'])
+    return factor.at[87, 'total_concentration']
 
 
-def set_concentration_value(x, changed_lipid, new_con):
+def set_concentration_value(x, changed_lipid, new_con, factor):
     # calculates and returns the new concentration for the lipid
-    if x["ID"] == changed_lipid:
+    if str(x['ID']) == changed_lipid:
         new_concentration = new_con*x["total_concentration"]
     else:
-        f = set_concentration_factor(x, changed_lipid, new_con)
-        new_concentration = x["total_concentration"]*f
+        new_concentration = x['total_concentration'] * factor
     return new_concentration
 
 
@@ -33,6 +33,7 @@ old_grid = pd.read_csv("C:\\Users\\hfComp\\Desktop\\WS\\lipidomics_with_chol.csv
 new_grid = pd.read_csv("C:\\Users\\hfComp\\Desktop\\WS\\lipidomics_with_chol.csv")
 for i in [4]:
     wanted_lipid = "Cer4412"
-    new_grid["updated_concentration"] = old_grid.apply(set_concentration_value(old_grid, wanted_lipid, i),
-                                                       axis="columns")
+    f = set_concentration_factor(old_grid, wanted_lipid, i)
+    new_grid["updated_concentration"] = old_grid.apply(set_concentration_value, changed_lipid=wanted_lipid, new_con=i,
+                                                       factor=f, axis="columns")
     new_grid.to_csv("C:\\Users\\hfComp\\Desktop\\WS\\new_composition_grid.csv")
