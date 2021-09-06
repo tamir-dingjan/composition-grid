0f0cf78f49714642ea40de3ae5884d77ccc41047
hfComp <Hila.flumin@gmail.com>
2021-09-06 15:24:16 +0300

    updated new grid file set up to update all layers according to the wanted one

--- composition_grid.py
+++ composition_grid.py
@@ -8,6 +8,7 @@ Created on Mon Aug 2 12:36:05 2021
 
 import numpy as np
 import pandas as pd
+# import json
 
 
 def set_concentration_factor(x, changed_lipid, membrane_part, new_con):
@@ -32,10 +33,26 @@ old_grid = pd.read_csv("C:\\Users\\hfComp\\Desktop\\WS\\lipidomics_with_chol.csv
 new_grid = pd.read_csv("C:\\Users\\hfComp\\Desktop\\WS\\lipidomics_with_chol.csv")
 membrane_parts = input("Choose membrane layer you want to change. \nFor both layers type 'total_concentration'."
                        "\nFor outer layer type 'outside'. \nFor inner layer type 'inside':")
-wanted_lipid = input("Insert lipid ID:")
+wanted_lipid = "Cer4412"
 for i in [3, 4, 5]:
-    f = set_concentration_factor(old_grid, wanted_lipid, membrane_parts, i)
-    t_sum = np.sum(old_grid[membrane_parts])
     new_grid[membrane_parts] = old_grid.apply(set_concentration_value, changed_lipid=wanted_lipid, new_con=i,
-                                              membrane_part=membrane_parts, total_sum=t_sum, factor=f, axis="columns")
+                                              membrane_part=membrane_parts,
+                                              total_sum=np.sum(old_grid[membrane_parts]),
+                                              factor=set_concentration_factor(old_grid, wanted_lipid, membrane_parts,
+                                                                              i), axis="columns")
+    if membrane_parts == "total_concentration":
+        new_grid["outside"] = old_grid.apply(set_concentration_value, changed_lipid=wanted_lipid, new_con=i,
+                                             membrane_part="outside",
+                                             total_sum=np.sum(old_grid["outside"]),
+                                             factor=set_concentration_factor(old_grid, wanted_lipid,
+                                                                             "outside",
+                                                                             i), axis="columns")
+        new_grid["inside"] = old_grid.apply(set_concentration_value, changed_lipid=wanted_lipid, new_con=i,
+                                            membrane_part="inside",
+                                            total_sum=np.sum(old_grid["inside"]),
+                                            factor=set_concentration_factor(old_grid, wanted_lipid,
+                                                                            "inside",
+                                                                            i), axis="columns")
+    else:
+        new_grid["total_concentration"] = new_grid["inside"].fillna(value=0) + new_grid["outside"].fillna(value=0)
     new_grid.to_csv("C:\\Users\\hfComp\\Desktop\\WS\\new_composition_grid" + str(i) + ".csv")
